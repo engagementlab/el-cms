3b08d59a163817e3c5d1e8fe4ae137639687dbdc
Johnny Richardson <johnny.c.rich@gmail.com>
2022-04-29 11:49:03 -0400

    refactor: barebones CMS.

--- keystone.ts
+++ keystone.ts
@@ -12,9 +12,9 @@ import { config } from '@keystone-6/core';
 import { lists } from './schema';
 
 // Keystone auth is configured separately - check out the basic auth setup we are importing from our auth file.
-import { withAuth, session } from './auth';
+// import { withAuth, session } from './auth';
 
-export default withAuth(
+export default
   // Using the config function helps typescript guide you to the available options.
   config({
     // the db sets the database provider - we're using sqlite for the fastest startup experience
@@ -23,11 +23,11 @@ export default withAuth(
       url: 'file:./keystone.db',
     },
     // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
-    ui: {
-      // For our starter, we check that someone has session data before letting them see the Admin UI.
-      isAccessAllowed: (context) => !!context.session?.data,
-    },
+    // ui: {
+    //   // For our starter, we check that someone has session data before letting them see the Admin UI.
+    //   isAccessAllowed: (context) => !!context.session?.data,
+    // },
     lists,
-    session,
+    // session,
   })
-);
+
